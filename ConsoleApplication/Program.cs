162a3fa5cb3cf7033af3f5fa1a2dac1bfe98fe30
lemonlime <416136521@qq.com>
2015-03-14 21:29:28 +0800

    fix market data bug.

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -7,6 +7,7 @@ using QuantBox.XAPI.Callback;
 using QuantBox;
 using QuantBox.XAPI;
 using System.Runtime.InteropServices;
+using System.Threading;
 namespace ConsoleApplication
 {
     class Program
@@ -14,8 +15,17 @@ namespace ConsoleApplication
         static void OnConnectionStatus(object sender, ConnectionStatus status, ref RspUserLoginField userLogin, int size1)
         {
             Console.WriteLine(status + userLogin.ErrorMsg());
+            if (status == ConnectionStatus.Done)
+            {
+                quoteApi.Subscribe("IF1212;IF1211;IF1210", "");
+            }
         }
         static void OnRtnError(object sender, [In] ref ErrorField error)
+        {
+
+        }
+
+        static void OnRtnQuote(object sender, [In] ref QuoteField quote)
         {
 
         }
@@ -27,20 +37,25 @@ namespace ConsoleApplication
             Console.WriteLine(marketData.ExchangeID);
             Console.WriteLine(marketData.LastPrice);
         }
+        public static XApi quoteApi = null;
         static void Main(string[] args)
         {
-            XApi quoteApi = new XApi(@"QuantBox_Femas_Quote.dll");
+            quoteApi = new XApi(@"QuantBox_Femas_Quote.dll");
             quoteApi.Server.Address = "tcp://117.184.207.108:6888"; //"tcp://124.74.248.150:7230";//
             quoteApi.User.UserID = "155092"; //"jk8";//
             quoteApi.User.Password = "666666"; //"111111";//
             quoteApi.Server.BrokerID = "0001"; //"0152";//
-
+            quoteApi.Server.TopicId = 100;
+            quoteApi.Server.MarketDataTopicResumeType = ResumeType.Quick;
+            
+            //quoteApi.SubscribedInstruments["S"] = new SortedSet<string>() { "IF1212;IF1211;IF1210" };
             quoteApi.OnConnectionStatus = OnConnectionStatus;
             quoteApi.OnRtnDepthMarketData = OnRtnDepthMarketData;
             quoteApi.OnRtnError = OnRtnError;
             quoteApi.Connect();
-            quoteApi.Subscribe("IF1211;IF1210;IF1212", "");
-
+            //Thread.Sleep(1000);
+            
+            //quoteApi.SubscribeQuote("IF1212", "");//IF1211;IF1210;44
             Console.ReadKey();
         }
     }
